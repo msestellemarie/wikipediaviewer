2dabaea5e7757a23452045b3483bdcfe5e2c55a8
Estelle Sillari-Owen <emso18@Estelles-MacBook-Pro.local>
2017-04-10 22:06:41 -0400

    updated api calls

--- wikipedia.js
+++ wikipedia.js
@@ -2,29 +2,35 @@ function searchResults(){
   $("form").on("submit", function(e){
     e.preventDefault();
     var search = $("input:text").val();
-    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=" + search + "&srqiprofile=wsum_inclinks_pv&callback=?", function(result){
-      for(var i=0; i<5; i++){
-        if(result.query.search[i].title === undefined){
-          $(".title"+(i+1)).empty();
-          $(".content"+(i+1)).empty();
+    if(search !== ""){
+      $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=search&exchars=140&exlimit=max&exintro=1&explaintext=1&gsrsearch=" + search + "&gsrnamespace=0&gsrlimit=5&gsrqiprofile=wsum_inclinks_pv&callback=?", function(result){
+        if(result.query === undefined){
+          alert("There are no results!");
+          $(".col-xl-6").hide();
+          $("input:text").val("");
         }
         else{
-          $(".title"+(i+1)).html("<a target='_blank' href='https://en.wikipedia.org/wiki/" + result.query.search[i].title + "'>" + result.query.search[i].title + "</a>");
-          $(".content"+(i+1)).html(result.query.search[i].snippet + "...");
-          $(".c"+(i+1)).show();
+          var count = 1;
+          for(var each in result.query.pages){
+            $(".title"+count).html("<a target='_blank' href='https://en.wikipedia.org/wiki/" + result.query.pages[each].title + "'>" + result.query.pages[each].title + "</a>");
+            $(".content"+count).html(result.query.pages[each].extract);
+            $(".c"+count).show();
+            count += 1;
+          }
+          $(".random-pg").hide();
         }
-      }
-      $(".random-pg").hide();
-    });
+      });
+    }
   });
 }
 
 function randomArticle(){
   $(".random").click(function(){
-    $.getJSON("https://en.wikipedia.org//w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=5&callback=?", function(random){
+    //switch to one request using extracts, if possible
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=random&rnnamespace=0&rnlimit=5&callback=?", function(random){
       $(".title-random").html("<a target='_blank' href='https://en.wikipedia.org/wiki/" + random.query.random[0].title + "'>" + random.query.random[0].title + "</a>");
-      $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srsearch=" + random.query.random[0].title + "&srqiprofile=wsum_inclinks_pv&callback=?", function(result){
-        $(".content-random").html(result.query.search[0].snippet + "...");
+      $.getJSON("https://en.wikipedia.org//w/api.php?action=query&format=json&prop=extracts&titles=" + random.query.random[0].title + "&exchars=140&exlimit=max&exintro=1&explaintext=1&callback=?", function(result){
+        $(".content-random").html(result.query.pages[Object.keys(result.query.pages)[0]].extract);
         $(".random-pg").show();
         $(".c1").hide();
         $(".c2").hide();
