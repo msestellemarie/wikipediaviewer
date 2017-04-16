a79f91240adee8876668d4163b3bf39eb314a820
Estelle Sillari-Owen <emso18@Estelles-MacBook-Pro.local>
2017-04-16 15:16:50 -0400

    fixed centering issues, added style

--- wikipedia.js
+++ wikipedia.js
@@ -1,15 +1,27 @@
 function searchResults(){
   $("form").on("submit", function(e){
+    $(".c1").hide();
+    $(".c2").hide();
+    $(".c3").hide();
+    $(".c4").hide();
+    $(".c5").hide();
     e.preventDefault();
+    $(".btn-outline-primary").blur();
     var search = $("input:text").val();
     if(search !== ""){
       $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&generator=search&exchars=140&exlimit=max&exintro=1&explaintext=1&gsrsearch=" + search + "&gsrnamespace=0&gsrlimit=5&gsrqiprofile=wsum_inclinks_pv&callback=?", function(result){
         if(result.query === undefined){
           alert("There are no results!");
-          $(".col-xl-6").hide();
+          $(".c1").hide();
+          $(".c2").hide();
+          $(".c3").hide();
+          $(".c4").hide();
+          $(".c5").hide();
           $("input:text").val("");
         }
         else{
+          $(".header").hide();
+          $(".container").css("justify-content","");
           var count = 1;
           for(var each in result.query.pages){
             $(".title"+count).html("<a target='_blank' href='https://en.wikipedia.org/wiki/" + result.query.pages[each].title + "'>" + result.query.pages[each].title + "</a>");
@@ -20,6 +32,17 @@ function searchResults(){
           $(".random-pg").hide();
         }
       });
+    }
+  });
+  $("input").keyup(function(){
+    if(!this.value){
+      $(".c1").hide();
+      $(".c2").hide();
+      $(".c3").hide();
+      $(".c4").hide();
+      $(".c5").hide();
+      $(".container").css("justify-content","center");
+      $(".header").show();
     }
   });
 }
@@ -35,13 +58,33 @@ function randomArticle(){
       $(".c3").hide();
       $(".c4").hide();
       $(".c5").hide();
+      $(".container").css("justify-content","");
       $("input:text").val("");
+      $(".btn-outline-primary").blur();
     });
+  });
+  $("input").keyup(function(){
+    if(!this.value){
+      $(".c1").hide();
+      $(".c2").hide();
+      $(".c3").hide();
+      $(".c4").hide();
+      $(".c5").hide();
+      $(".random-pg").hide();
+      $(".container").css("justify-content","center");
+    }
   });
 }
 
 $(document).ready(function(){
-  $(".col-xl-6").hide();
+  $(".c1").hide();
+  $(".c2").hide();
+  $(".c3").hide();
+  $(".c4").hide();
+  $(".c5").hide();
+  $(".random-pg").hide();
+  $(".container").css("justify-content","center");
+  $(".header").show();
   searchResults();
   randomArticle();
 });
