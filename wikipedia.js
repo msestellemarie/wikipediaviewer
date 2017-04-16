4a05486419dcfa5e469563f3832eade15eb29096
Estelle Sillari-Owen <emso18@gmail.com>
2017-04-16 16:15:55 -0400

    fixed url issues with escape

--- wikipedia.js
+++ wikipedia.js
@@ -24,7 +24,7 @@ function searchResults(){
           $(".container").css("justify-content","");
           var count = 1;
           for(var each in result.query.pages){
-            $(".title"+count).html("<a target='_blank' href='https://en.wikipedia.org/wiki/" + result.query.pages[each].title + "'>" + result.query.pages[each].title + "</a>");
+            $(".title"+count).html("<a target='_blank' href='https://en.wikipedia.org/wiki/" + escape(result.query.pages[each].title) + "'>" + result.query.pages[each].title + "</a>");
             $(".content"+count).html(result.query.pages[each].extract);
             $(".c"+count).show();
             count += 1;
@@ -50,7 +50,7 @@ function searchResults(){
 function randomArticle(){
   $(".random").click(function(){
     $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts&list=&generator=random&utf8=1&exchars=140&exintro=1&explaintext=1&grnnamespace=0&callback=?", function(random){
-      $(".title-random").html("<a target='_blank' href='https://en.wikipedia.org/wiki/" + random.query.pages[Object.keys(random.query.pages)[0]].title + "'>" + random.query.pages[Object.keys(random.query.pages)[0]].title + "</a>");
+      $(".title-random").html("<a target='_blank' href='https://en.wikipedia.org/wiki/" + escape(random.query.pages[Object.keys(random.query.pages)[0]].title) + "'>" + random.query.pages[Object.keys(random.query.pages)[0]].title + "</a>");
       $(".content-random").html(random.query.pages[Object.keys(random.query.pages)[0]].extract);
       $(".random-pg").show();
       $(".c1").hide();
